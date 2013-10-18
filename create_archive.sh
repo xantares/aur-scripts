df0a08f7e8a1a3b104c441e35229267a0817caa7
xantares <xantares09@hotmail.com>
2013-10-18 19:07:47 +0000

    +binutils

--- create_archive.sh
+++ create_archive.sh
@@ -23,7 +23,7 @@ set -e
 
 rm -rf mingw-w64-toolchain*
 mkdir mingw-w64-toolchain
-for pkgname in mingw-w64-headers mingw-w64-crt mingw-w64-winpthreads mingw-w64-gcc mingw-w64-lapack mingw-w64-cmake mingw-w64-libxml2 mingw-w64-libgnurx mingw-w64-intel-tbb mingw-w64-dlfcn mingw-w64-gettext mingw-w64-termcap mingw-w64-libiconv mingw-w64-zlib
+for pkgname in mingw-w64-binutils mingw-w64-headers mingw-w64-crt mingw-w64-winpthreads mingw-w64-gcc mingw-w64-lapack mingw-w64-cmake mingw-w64-libxml2 mingw-w64-libgnurx mingw-w64-intel-tbb mingw-w64-dlfcn mingw-w64-gettext mingw-w64-termcap mingw-w64-libiconv mingw-w64-zlib
 do
   echo "-- $pkgname"
   files=`pacman -Ql $pkgname | cut -d " " -f 2`
@@ -36,5 +36,6 @@ do
   done
 done
 
+echo "-- archive..."
 tar czf mingw-w64-toolchain.tar.gz mingw-w64-toolchain/*
 
