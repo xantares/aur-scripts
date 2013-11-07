76a1ef48fe4f2c0604328efdade21bf0b3d8c4f3
xantares <xantares09@hotmail.com>
2013-11-07 08:08:12 +0000

    -libiconv

--- install_mingw.sh
+++ install_mingw.sh
@@ -2,36 +2,12 @@
 
 set -e
 
-#remove: yaourt -Rscnd mingw-w64
+#remove all: yaourt -Rscnd mingw-w64
 
-#yaourt -Sya mingw-w64-binutils --noconfirm
-#yaourt -S mingw-w64-crt --noconfirm
+yaourt -Sya mingw-w64-binutils --noconfirm
+yaourt -S mingw-w64-crt --noconfirm
 
 function install_conflict {
-  cat > /tmp/conflict.sh << EOF
-#!/usr/bin/expect -f
-set timeout -1
-spawn yaourt -S $1
-expect "Edit PKGBUILD ?"
-sleep 1
-send "n"
-expect "Continue building $1"
-sleep 1
-send "Y"
-expect "Continue installing $1"
-sleep 1
-send "Y"
-expect " are in conflict. Remove "
-sleep 1
-send "y\r"
-expect "Proceed with installation"
-sleep 1
-send "Y\r"
-#expect "overwrite"
-#sleep 1
-#send "y\r"
-expect eof
-EOF
 
 cat > /tmp/conflict.sh << EOF
 #!/usr/bin/expect -f
@@ -49,24 +25,20 @@ chmod a+rx /tmp/conflict.sh
 /tmp/conflict.sh
 }
 
-
-#install_conflict mingw-w64-winpthreads
-#install_conflict mingw-w64-gcc
-
 pushd /tmp
 
 rm -rf mingw-w64-winpthreads
 yaourt -G mingw-w64-winpthreads 
 pushd mingw-w64-winpthreads
-#makepkg --asroot
-#install_conflict mingw-w64-winpthreads-3.0.0-2-any.pkg.tar.xz
+makepkg --asroot
+install_conflict mingw-w64-winpthreads-3.0.0-2-any.pkg.tar.xz
 popd
 
 rm -rf mingw-w64-gcc
 yaourt -G mingw-w64-gcc 
 pushd mingw-w64-gcc
 makepkg --asroot
-install_conflict mingw-w64-gcc-4.8.2-2-any.pkg.tar.xz
+install_conflict mingw-w64-gcc-4.8.2-2-x86_64.pkg.tar.xz
 popd
 
 
