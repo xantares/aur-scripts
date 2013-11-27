d7234b50be5fc8a75a5007ccbb3460ebf2d035d2
xantares <xantares@fujitsu-l64>
2013-11-27 19:18:42 +0100

    +swt +boost

--- install_mingw.sh
+++ install_mingw.sh
@@ -4,9 +4,6 @@ set -e
 
 #remove all: yaourt -Rscnd mingw-w64
 
-yaourt -Sya mingw-w64-binutils --noconfirm
-yaourt -S mingw-w64-crt --noconfirm
-
 function install_conflict {
 
 cat > /tmp/conflict.sh << EOF
@@ -25,28 +22,12 @@ chmod a+rx /tmp/conflict.sh
 /tmp/conflict.sh
 }
 
-pushd /tmp
-
-rm -rf mingw-w64-winpthreads
-yaourt -G mingw-w64-winpthreads 
-pushd mingw-w64-winpthreads
-makepkg --asroot
-install_conflict mingw-w64-winpthreads-3.0.0-2-any.pkg.tar.xz
-popd
-
-rm -rf mingw-w64-gcc
-yaourt -G mingw-w64-gcc 
-pushd mingw-w64-gcc
-makepkg --asroot
-install_conflict mingw-w64-gcc-4.8.2-3-x86_64.pkg.tar.xz
-popd
-
-
+yaourt -Sya mingw-w64-gcc --noconfirm
 yaourt -S mingw-w64-lapack --noconfirm
 yaourt -S mingw-w64-libgnurx --noconfirm
 yaourt -S mingw-w64-cmake --noconfirm
 yaourt -S mingw-w64-libxml2 --noconfirm
-yaourt -S mingw-w64-intel-tbb --noconfirm
+# yaourt -S mingw-w64-intel-tbb --noconfirm
 yaourt -S mingw-w64-muparser --noconfirm
 yaourt -S mingw-w64-libsvm --noconfirm
 yaourt -S mingw-w64-fftw --noconfirm
