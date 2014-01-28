ad4644048c20adc6f5ea8f987a4ad30ad4b1551d
xantares <xantares09@hotmail.com>
2014-01-28 13:29:00 +0000

    updated list

--- create_archive.sh
+++ create_archive.sh
@@ -6,7 +6,7 @@ arch=x86_64
 
 rm -rf mingw-w64-$arch*
 mkdir mingw-w64-$arch
-for pkgname in mingw-w64-binutils mingw-w64-headers mingw-w64-crt mingw-w64-winpthreads mingw-w64-gcc mingw-w64-lapack mingw-w64-cmake mingw-w64-libxml2 mingw-w64-libgnurx mingw-w64-dlfcn mingw-w64-gettext mingw-w64-termcap mingw-w64-libiconv mingw-w64-zlib mingw-w64-muparser mingw-w64-libsvm mingw-w64-fftw
+for pkgname in mingw-w64-binutils mingw-w64-headers mingw-w64-crt mingw-w64-winpthreads mingw-w64-gcc mingw-w64-lapack mingw-w64-cmake mingw-w64-libxml2 mingw-w64-libgnurx mingw-w64-dlfcn mingw-w64-gettext mingw-w64-termcap mingw-w64-libiconv mingw-w64-zlib mingw-w64-muparser mingw-w64-libsvm mingw-w64-fftw mingw-w64-mixmod mingw-w64-python2
 do
   echo "-- $pkgname"
   files=`pacman -Ql $pkgname | cut -d " " -f 2`
@@ -18,10 +18,6 @@ do
     fi
   done
 done
-
-# only static
-#rm mingw-w64-$arch/usr/*-w64-mingw32/bin/*.dll
-#rm mingw-w64-$arch/usr/*-w64-mingw32/lib/*.dll.a
 
 echo "-- archive..."
 tar czf mingw-w64-$arch.tar.gz mingw-w64-$arch/*
--- install_mingw.sh
+++ install_mingw.sh
@@ -33,3 +33,5 @@ yaourt -S mingw-w64-libsvm --noconfirm
 yaourt -S mingw-w64-fftw --noconfirm
 yaourt -S mingw-w64-mixmod --noconfirm
 yaourt -S mingw-w64-dlfcn  --noconfirm
+yaourt -S mingw-w64-python2 --noconfirm
+
