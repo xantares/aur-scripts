6c3ad769fb595311fe40660f86f179bb0c08933f
xantares <xantares09@hotmail.com>
2014-07-15 10:09:52 +0000

    +tbb +fann

--- install_mingw.sh
+++ install_mingw.sh
@@ -32,7 +32,9 @@ yaourt -S mingw-w64-muparser --noconfirm
 yaourt -S mingw-w64-libsvm --noconfirm
 yaourt -S mingw-w64-fftw --noconfirm
 yaourt -S mingw-w64-mixmod --noconfirm
+yaourt -S mingw-w64-fann --noconfirm
 yaourt -S mingw-w64-dlfcn  --noconfirm
+yaourt -S mingw-w64-intel-tbb --noconfirm
 yaourt -S mingw-w64-python-native --noconfirm
 yaourt -S mingw-w64-python2-native --noconfirm
 yaourt -S mingw-w64-python26-native --noconfirm
