4d9c50c71cf1e5ad07001887f4ca1a02178ee5e2
xantares <xantares09@hotmail.com>
2014-10-06 09:01:23 +0000

    +fftw

--- install_mingw.sh
+++ install_mingw.sh
@@ -24,18 +24,18 @@ chmod a+rx /tmp/conflict.sh
 
 yaourt -Syu --noconfirm
 yaourt -S mingw-w64-gcc --noconfirm
-yaourt -S mingw-w64-lapack --noconfirm
-yaourt -S mingw-w64-libgnurx --noconfirm
-yaourt -S mingw-w64-cmake --noconfirm
-yaourt -S mingw-w64-libxml2 --noconfirm
+yaourt -S mingw-w64-dlfcn  --noconfirm
 yaourt -S mingw-w64-muparser --noconfirm
 yaourt -S mingw-w64-libsvm --noconfirm
+yaourt -S mingw-w64-cmake --noconfirm
+yaourt -S mingw-w64-fann --noconfirm
+yaourt -S mingw-w64-intel-tbb --noconfirm
+yaourt -S mingw-w64-lapack --noconfirm
+yaourt -S mingw-w64-libgnurx --noconfirm
+yaourt -S mingw-w64-libxml2 --noconfirm
 yaourt -S mingw-w64-fftw --noconfirm
 yaourt -S mingw-w64-mixmod --noconfirm
 yaourt -S mingw-w64-agrum --noconfirm
-yaourt -S mingw-w64-fann --noconfirm
-yaourt -S mingw-w64-dlfcn  --noconfirm
-yaourt -S mingw-w64-intel-tbb --noconfirm
 yaourt -S mingw-w64-python-bin --noconfirm
 yaourt -S mingw-w64-python2-bin --noconfirm
 yaourt -S mingw-w64-python26-bin --noconfirm
