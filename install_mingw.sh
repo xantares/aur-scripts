f29b477b558eac6f7f02a226bdb0a7fba39b8140
xantares <xantares09@hotmail.com>
2014-10-23 10:36:31 +0000

    +cmake

--- install_mingw.sh
+++ install_mingw.sh
@@ -27,21 +27,20 @@ yaourt -S mingw-w64-gcc --noconfirm
 yaourt -S mingw-w64-dlfcn  --noconfirm
 yaourt -S mingw-w64-muparser --noconfirm
 yaourt -S mingw-w64-libsvm --noconfirm
-yaourt -S mingw-w64-cmake --noconfirm
 yaourt -S mingw-w64-fann --noconfirm
-yaourt -S mingw-w64-intel-tbb --noconfirm
-yaourt -S mingw-w64-lapack --noconfirm
 yaourt -S mingw-w64-libgnurx --noconfirm
-yaourt -S mingw-w64-libxml2 --noconfirm
-yaourt -S mingw-w64-fftw --noconfirm
+yaourt -S mingw-w64-intel-tbb --noconfirm
 yaourt -S mingw-w64-mixmod --noconfirm
+yaourt -S mingw-w64-libxml2 --noconfirm
+yaourt -S mingw-w64-lapack --noconfirm
+yaourt -S mingw-w64-fftw --noconfirm
 yaourt -S mingw-w64-agrum --noconfirm
 yaourt -S mingw-w64-python-bin --noconfirm
 yaourt -S mingw-w64-python2-bin --noconfirm
 yaourt -S mingw-w64-python26-bin --noconfirm
 yaourt -S mingw-w64-boost --noconfirm
 #yaourt -S mingw-w64-qt4 --noconfirm
-yaourt -G mingw-w64-qt4 --noconfirm && cd mingw-w64-qt4 && makepkg --asroot && pacman -U mingw-w64-qt4-*-any.pkg.tar.xz && cd -
+yaourt -G mingw-w64-qt4 --noconfirm && cd mingw-w64-qt4 && makepkg --asroot && pacman -U mingw-w64-qt4-*-any.pkg.tar.xz && rm -r src 
 yaourt -S mingw-w64-qscintilla --noconfirm
 yaourt -S mingw-w64-qwt5 --noconfirm
 yaourt -S mingw-w64-iistaskpanel --noconfirm
