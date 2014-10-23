a9c2a7ccd754c7792fe30b65e873059c180b60e7
xantares <xantares09@hotmail.com>
2014-10-23 08:13:45 +0000

    +mingw-configure

---
+++ mingw-w64-configure/mingw-configure.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export CFLAGS="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4"
+export CXXFLAGS="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4"
+../configure --host=@TRIPLE@ --enable-shared --enable-static --prefix=/usr/@TRIPLE@ "$@"
+
