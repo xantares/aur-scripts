312fd7c6f31e464dc551e358bc194d2607ba803c
Michel Zou <xantares09@hotmail.com>
2016-11-28 19:24:59 +0100

    update migrate script

--- migrate.sh
+++ migrate.sh
@@ -4,13 +4,16 @@ set -e
 
 pkgs=$*
 
-if ! test -d aur-mirror
-then
-  git clone --depth 1 git://pkgbuild.com/aur-mirror.git
-fi
+#if ! test -d aur-mirror
+#then
+#  #git clone --depth 1 git://pkgbuild.com/aur-mirror.git
+#  git clone --depth 1 https://github.com/felixonmars/aur3-mirror.git aur-mirror
+#fi
 
 for pkg in $pkgs
 do
+  mkdir -p aur-mirror
+  git clone https://github.com/aur-archive/$pkg.git aur-mirror/${pkg}
   if test -f "aur-mirror/$pkg/PKGBUILD"
   then
     git clone ssh://aur@aur.archlinux.org/$pkg.git
