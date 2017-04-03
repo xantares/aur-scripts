2dbc3660013782edd409e24901c1a728ddf80e32
Michel Zou <xantares09@hotmail.com>
2017-04-03 22:40:01 +0200

    +lpsolve, qt5

--- install_mingw.sh
+++ install_mingw.sh
@@ -6,8 +6,6 @@ set -e
 #patch -p0 -i /usr/share/makepkg-asroot/makepkg-asroot.patch
 
 
-
-
 function install_conflict {
 
 cat > /tmp/conflict.sh << EOF
@@ -27,6 +25,7 @@ chmod a+rx /tmp/conflict.sh
 }
 
 gpg --recv D605848ED7E69871 D9C4D26D0E604491 0223A078DBFF4B66 BB5869F064EA74AB # gettext, openssl, dbus, readline
+gpg --recv C1A60EACE707FDA5 9766E084FB0F43D8 38EE757D69184620 # freetype2, pcre, xz
 
 # remove all
 yaourt -Rscnd mingw-w64 --noconfirm || echo "already clean" 
@@ -40,6 +39,7 @@ yaourt -S mingw-w64-dlfcn  --noconfirm
 yaourt -S mingw-w64-libsvm --noconfirm
 yaourt -S mingw-w64-libgnurx --noconfirm
 yaourt -S mingw-w64-muparser --noconfirm
+yaourt -S mingw-w64-lpsolve --noconfirm
 yaourt -S mingw-w64-fann --noconfirm
 yaourt -S mingw-w64-intel-tbb --noconfirm
 yaourt -S mingw-w64-libmixmod --noconfirm
@@ -56,9 +56,22 @@ yaourt -S mingw-w64-python34-bin --noconfirm
 yaourt -S mingw-w64-python33-bin --noconfirm
 yaourt -S mingw-w64-boost --noconfirm --tmp $PWD
 yaourt -S mingw-w64-agrum --noconfirm
+
 yaourt -S mingw-w64-libzip --noconfirm
-yaourt -S mingw-w64-qt4 --noconfirm --tmp $PWD || { rm /usr/*-w64-mingw32/bin/Qt*.dll; pacman -U yaourt-tmp-root/mingw-w64-qt4-*.pkg.tar.xz --noconfirm; rm -r yaourt-tmp-root; }
+yaourt -S mingw-w64-qt4 --noconfirm --tmp $PWD || { rm /usr/*-w64-mingw32/bin/Qt*4.dll; pacman -U yaourt-tmp-root/mingw-w64-qt4-*.pkg.tar.xz --noconfirm; rm -r yaourt-tmp-root; }
 yaourt -S mingw-w64-qscintilla-qt4 --noconfirm
 yaourt -S mingw-w64-qwt --noconfirm
 yaourt -S mingw-w64-iistaskpanel --noconfirm
 
+
+yaourt -S mingw-w64-freetype2-bootstrap --noconfirm
+yaourt -S mingw-w64-cairo-bootstrap --noconfirm
+yaourt -S mingw-w64-freetype2 --noconfirm || install_conflict /tmp/yaourt-tmp-root/mingw-w64-freetype2*.xz
+yaourt -S mingw-w64-cairo --noconfirm || install_conflict /tmp/yaourt-tmp-root/mingw-w64-cairo*.xz
+yaourt -S mingw-w64-qt5-base --noconfirm --tmp $PWD
+yaourt -S mingw-w64-qt5-base-static --noconfirm --tmp $PWD
+yaourt -S mingw-w64-qt5-xmlpatterns --noconfirm --tmp $PWD
+yaourt -S mingw-w64-qt5-declarative --noconfirm --tmp $PWD
+yaourt -S mingw-w64-qt5-tools --noconfirm --tmp $PWD
+yaourt -S mingw-w64-paraview --noconfirm --tmp $PWD
+# yaourt -S mingw-w64-qwt-qt5 --noconfirm
