4f7fb6e0ba87498b274de75da743342d48db7a1f
Michel Zou <xantares09@hotmail.com>
2019-02-22 19:48:38 +0100

    update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+if test "$#" -lt 2
+then
+  echo "usage: $0 pkgname pkgver"
+  exit
+fi
+
+pkgname=$1
+pkgver=$2
+
+if ! test -d ${pkgname}
+then
+  echo "${pkgname} do not exist"
+  exit
+fi
+
+oldver=`cat ${pkgname}/PKGBUILD|grep 'pkgver=' |sed "s|pkgver=\(.*\)|\\1|g"`
+
+echo "-- Updating ${pkgname}-${oldver} => ${pkgver}"
+sed -i "s|pkgver=${oldver}|pkgver=${pkgver}|g" ${pkgname}/PKGBUILD
+pushd ${pkgname} && makepkg -g > /tmp/checksum && popd
+checksum_type=`cat /tmp/checksum | grep 'sums='| sed "s|\(.*\)sums=.*|\\1|g"`
+checksum=`cat /tmp/checksum`
+sed -i "s|${checksum_type}sums=(.*)|${checksum}|g" ${pkgname}/PKGBUILD
+pushd ${pkgname} && git diff && popd
+echo "-- If ok: ./put.sh ${pkgname} ${pkgver}"
